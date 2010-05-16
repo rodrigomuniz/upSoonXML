338a5af14c998c4f6b947971e0d065308c4c61d5
Rodrigo Muniz <eu@rodrigomuniz.com>
2010-05-16 08:34:34 +0000

    * Finalizando produção e testes da versão 0.5 * Melhorias na legibilidade do código * Implementação da opção de inserir o Google Analytics * Data de lançamento, se inserida, agora aparece também no <title> * CSS compactado para deploy

--- index.php
+++ index.php
@@ -1,71 +1,91 @@
 <?php
 /*
- * UpSoonXML v 0.1
- * http://wenetus.com
+ * UpSoonXML v 0.5
+ * http://code.google.com/p/upsoonxml/
  *
- * Copyright 2010, Rodrigo Muniz - Wenetus Interactive
+ * Copyright 2010, Rodrigo Muniz - Wenetus Interactive http://wenetus.com
  * Dual licensed under the MIT or GPL Version 2 licenses.
  *
  */
-
+ 
 require_once('functions.php');
 
-// ** CONFIGURACOES ** //
+// ======================================
+// ! CONFIGURACOES   
+// ======================================
 /** 1. Nome do arquivo XML que sera usado como db para armazenar os assinantes */
 /** IMPORTANTE: por questoes de seguranca voce DEVE renomear o arquivo para um nome que nao seja facil de descobrir e informar o novo nome abaixo */
-$dbname = "subscribers.xml";
+$dbName = "subscribers.xml";
 
+// ====================================== 
 /** 2. Chave de seguranca */
-/** Acesse http://www.adamek.biz/md5-generator.php e gere sua chave */
-/** Usado para que POSTs externos nao consigam inserir nada no seu xml */
-$yourkey = "5989f2c25d559f0acf08e541944182dd";
+/** Acesse http://www.adamek.biz/md5-generator.php e gere uma chave */
+/** Usado para que POSTs feitos por formulario externos nao consigam inserir nada no seu xml */
+$yourKey = "5989f2c25d559f0acf08e";
 
+// ====================================== 
 /** 3. Nome do site ou da empresa */
-$sitename = "ACME";
+$siteName = "ACME";
 
+// ====================================== 
 /** 4. Campo de Nome obrigatorio? */
 /** false = (Opcional) true = (Obrigatorio) */
-$nreq = false;
+$nameReq = false;
 
+// ====================================== 
 /** 5. URL da imagem para usar como Logotipo */
 /** vazio = (Oculta logo) */
 $logo = 'http://labs.rodrigomuniz.com/upsoonXML/logo.png';
 
-/** 6. Indexar nos Mercanismos de Busca como Google? */
+// ====================================== 
+/** 6. Indexar nos mercanismos de busca como o Google? */
 /** false = (Nao) true = (Sim) */
 $index = true;
 
-/** 7. Data de lançamento? */
+// ====================================== 
+/** 7. Temos uma data de lançamento? */
 /** vazio = (Oculta data), ex.: 01.06.2010 */
 $when = "";
 
-/** 8. Mostrar titulo e/ou subtitulo? */
+// ====================================== 
+/** 8. Google Analytics UA */
+/** vazio = (Nao usa Analytics) */
+$gaUA = "";
+
+// ====================================== 
+/** 9. Mostrar titulo e/ou subtitulo? */
 /** false = (Nao) true = (Sim) */
 $h1 = true;
 $h2 = true;
 
-/** Pronto, pode parar de editar o PHP */
+// ====================================== 
+// ! Pronto, pode parar de editar o PHP.
+// ! Edicoes feitas abaixo podem quebrar as coisas.
+// ====================================== 
 
 
 
 
 
 
+// ============================================================================ 
 // mensagens
 $e = false;
 $sent = false;
 
 //checa se xml ta pronto
-if(!file_exists($dbname)) {
+if(!file_exists($dbName)) {
 	$e .= "<li>ATENÇÃO. O arquivo XML para armazenar os assinantes não foi encontrado.</li>";
 } else { //xml encontrado
 
+// ====================================== 
+// AJAX
 if(isset($_POST['ax'])) {//se submeteu via ajax ignora o resto do php
 		$token = $_POST['token'];
-	if(is_token_valid($token, $yourkey)) {
+	if(is_token_valid($token, $yourKey)) {
 		$email = trim($_POST['email']);
 		$name = trim($_POST['name']);
-		$xml = new SimpleXMLElement($dbname, 0, true);
+		$xml = new SimpleXMLElement($dbName, 0, true);
 
 		//email em branco?
 		if($email == "") {
@@ -85,7 +105,7 @@ if(isset($_POST['ax'])) {//se submeteu via ajax ignora o resto do php
 }
 
 		//nome em branco?
-		if ($nreq==true) {//opcao de nome obrigatorio
+		if ($nameReq==true) {//opcao de nome obrigatorio
 			if($name == "") {
 				$e.= "<li>Por favor, preencha o campo de nome</li>";
 			}
@@ -93,12 +113,12 @@ if(isset($_POST['ax'])) {//se submeteu via ajax ignora o resto do php
 		
 		//se nao ha erros entao adiciona o email e o nome
 		if($e == false){
-			$xml = new SimpleXMLElement($dbname, 0, true);
+			$xml = new SimpleXMLElement($dbName, 0, true);
 			$user = $xml[0]->addChild('user');
 			$user->addChild('email', $email);
 			$user->addChild('realname', $name);
 			$user->addChild('time', time());
-			$xml->asXML($dbname);
+			$xml->asXML($dbName);
 
 			$sent = true;
 
@@ -108,14 +128,15 @@ if(isset($_POST['ax'])) {//se submeteu via ajax ignora o resto do php
 	}//token
 } else {
 
-//sem ajax
-		$secret = auth_token($yourkey);
+// ====================================== 
+// SEM AJAX
+		$secret = auth_token($yourKey);
 	if(isset($_POST['send'])) {
 		$token = $_POST['token'];
-	if(is_token_valid($token, $yourkey)) {
+	if(is_token_valid($token, $yourKey)) {
 		$email = trim($_POST['email']);
 		$name = trim($_POST['name']);
-		$xml = new SimpleXMLElement($dbname, 0, true);
+		$xml = new SimpleXMLElement($dbName, 0, true);
 		
 		//email em branco?
 		if($email == "") {
@@ -135,7 +156,7 @@ if(isset($_POST['ax'])) {//se submeteu via ajax ignora o resto do php
 		}
 
 		//nome em branco?
-		if ($nreq==true) {//opcao de nome obrigatorio
+		if ($nameReq==true) {//opcao de nome obrigatorio
 			if($name == "") {
 				$e.= "<li>Por favor, preencha o campo de nome</li>";
 			}
@@ -143,12 +164,12 @@ if(isset($_POST['ax'])) {//se submeteu via ajax ignora o resto do php
 
 		//se nao ha erros entao adiciona o email e o nome
 		if($e == false){
-			$xml = new SimpleXMLElement($dbname, 0, true);
+			$xml = new SimpleXMLElement($dbName, 0, true);
 			$user = $xml[0]->addChild('user');
 			$user->addChild('email', $email);
 			$user->addChild('realname', $name);
 			$user->addChild('time', time());
-			$xml->asXML($dbname);
+			$xml->asXML($dbName);
 
 			$sent = true;
 		}
@@ -157,12 +178,13 @@ if(isset($_POST['ax'])) {//se submeteu via ajax ignora o resto do php
   }//se submeteu com ajax
 }//se xml existe
 
-if(!isset($_POST['ax'])) { //nao escrever pagina no ajax ?>
+if(!isset($_POST['ax'])) { //nao escrever a pagina no retorno do ajax
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="pt-br">
 <head profile="http://gmpg.org/xfn/11">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Novo site em breve... - <?=$sitename?></title>
+	<title>Novo site em <? if($when){?><?=$when?><? } else { ?>breve...<? }?> - <?=$siteName?></title>
 <meta name="author" content="Wenetus Interactive - www.wenetus.com" />
 <? if(!$index){ ?>
 <meta name="robots" content="noindex, nofollow" />
@@ -170,8 +192,17 @@ if(!isset($_POST['ax'])) { //nao escrever pagina no ajax ?>
 <meta http-equiv="X-UA-Compatible" content="chrome=1" />
 <meta http-equiv="imagetoolbar" content="no" />
 
-<? /*CSS no header para nao precisar de mais um arquivo no servidor*/ ?>
+<?
+// ======================================
+// CSS no header para nao precisar de mais um arquivo no servidor
+// comprimido em http://refresh-sf.com/yui/
+?>
 <style type="text/css">
+html{border-top:10px solid #f0f0f0}body{background-color:#FFF;font-family:Helvetica,Arial,sans-serif;font-size:19px;color:#555;margin:0 auto;padding:5px 0;text-align:center;max-width:100%;_width:960px}img{border:0}form{text-align:left;margin:0 auto;padding:25px 3px;width:450px}fieldset{border:1px solid #ddd;-moz-border-radius:10px;-khtml-border-radius:10px;-webkit-border-radius:10px;border-radius:10px;padding:20px 0}legend{color:#aaa;text-align:center;margin:0 auto;padding:10px 0 0 0}fieldset ul{list-style:none;padding:0;margin:0}fieldset li{float:left;clear:both;padding:6px 0;position:relative;width:100%}fieldset label{display:block;font-size:.8em;font-weight:bold;text-align:right;padding:6px 4px 0 0;width:100px}fieldset label,input{float:left;vertical-align:middle}#email,#name{font-size:.9em;width:260px}form .sub{clear:both;display:block;float:right;cursor:pointer;font-size:12px;font-weight:bold;margin-right:73px}#alert{border-bottom:4px solid #f0f0f0;background-color:#ffd55c;color:#000;font-size:.9em;margin:0 auto;padding:10px;padding-bottom:0;position:fixed;top:0;left:-180px;margin-left:50%;text-align:left;width:360px;cursor:pointer}ul#alert{list-style:none}ul#alert li{padding-bottom:10px}.priv{font-size:11px;padding:4px 70px 6px 103px;margin:0 auto;width:271px;background:#f6f6f6;float:left;clear:both}h1,h2{font-family:'Helvetica Neue UltraLight','HelveticaNeue-UltraLight','Century Gothic',Helvetica,Arial,sans-serif;font-weight:normal;margin:-5px 0 -10px 0;padding:0}h1{color:#aaa;font-size:65px;text-transform:uppercase}h2{font-size:28px}h2 strong{color:#000}.opc{display:none}.hint{display:block;position:absolute;top:14px;right:84px;font-size:12px;cursor:text;color:#8e8e8e;z-index:100}
+<?
+// ======================================
+//CSS nao comprimido para producao
+/*
 html{border-top: 10px solid #f0f0f0}
 body {
 	background-color: #FFF;
@@ -284,7 +315,7 @@ h2{font-size: 28px}
 h2 strong {
 	color: #000;
 }
-.opc {display: none; /*jquery*/}
+.opc {display: none;}
 .hint {
 	display: block;
 	position: absolute;
@@ -294,11 +325,13 @@ h2 strong {
 	cursor: text;
 	color: #8e8e8e;
 	z-index: 100;
-}
+} */ ?>
 </style>
 
 <?php
-if ( strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') ){ //if IE ?>
+// ====================================== 
+// if IE 
+if ( strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') ){ ?>
 <!--[if lt IE 8]>
 <style type="text/css">
 .priv {
@@ -340,25 +373,32 @@ form .sub {
 form .sub {margin-right: 79px}
 </style>
 <![endif]-->
-<? } //if IE ?>
+<?
+}
+//if IE
+?>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 <script type="text/javascript">
 $(document).ready(function() {
+<?
+// ====================================== 
+// JS AJAX
+?>
 function remAlert() {
-	$('#alert').fadeOut('fast').remove('#alert');
-	$('#email').focus();
+	if($('#alert')){
+		$('#alert').fadeOut('fast').remove('#alert');
+		$('#email').focus();
+	}
 }
-<? // ajax ?>
-$('#loading').hide();
 var erBlock = "<ul id='alert' title='Fechar'></ul>";
 
 function processReturn(data){
 	if(data == 1) {
-		if($("#alert").remove("#alert"));
-				$("form fieldset").slideUp();
-				$("form").before("<p>Oba! Você está inscrito e será avisado no lançamento.</p>");
+		remAlert();
+		$("form fieldset").slideUp();
+		$("form").before("<p>Oba! Você está inscrito e será avisado no lançamento.</p>");
 	} else {
-		if($("#alert").remove("#alert"));
+		remAlert() 
 		$('body').append(erBlock);
 		$("#alert").append(data);
 		animAlert();
@@ -368,11 +408,14 @@ function processReturn(data){
 
 $('.sub').click(function(){
         $('.sub').ajaxStart(function(){
-			$(this).val('Analisando...').css('color', '#ff0000');
+        	$(this).attr('disabled', 'disabled');
+        	remAlert();
+       		$('body').append(erBlock);
+        	$("#alert").append('<li>Analisando...</li>');
         });
         $('.sub').ajaxStop(function(){
-            $(this).val('Sim. Quero ser avisado.').css('color', '#000');   
-        });
+        	$(this).removeAttr('disabled');
+       	});
         $.post('index.php',
         {ax:true, token: $('#token').val(), email: $('#email').val(), name: $('#name').val()},
         function(data){
@@ -380,15 +423,20 @@ $('.sub').click(function(){
                 	processReturn(data);
                 }
                 else{
-                	alert('O servidor não respondeu ao envio, por favor tente novamente.');
+		            remAlert();
+		       		$('body').append(erBlock);
+		        	$("#alert").append('<li>Houve uma falha na comunicação com o servidor. Você está conectado? Por favor tente novamente.</li>');
                 }
         });
 return false;
 });
 
-<?php if(!$e){ //animacao de respiracao ?>
+<?
+// ====================================== 
+// Animacao e respiracao do aviso de SPAM
+?>
+<?php if(!$e){ ?>
 $("form, .priv").hide(); <? //oculta inicialmente ?>
-
 function toFade() { <? //diminui a opacidade ?>
 	$(".priv").animate({opacity: '+=-0.5'}, 2000, function() {
 		resetFade();
@@ -406,6 +454,10 @@ $("form").slideDown(600,function(){
 });
 <? } ?>
 
+<?
+// ====================================== 
+// Animacao do alerta amarelo
+?>
 function animAlert() {
 	$("#alert").animate({left: '+=-16'}, 70)
 		.animate({left: '+=8'}, 70)
@@ -415,7 +467,11 @@ function animAlert() {
 		.animate({left: '+=4'}, 70);
 }
 
-<? if($nreq==false) {//hint ?>
+<?
+// ====================================== 
+// Comportamento do hint com o texto (Opcional) do campo de Nome
+?>
+<? if($nameReq==false) { ?>
 $(".opc").addClass('hint').click(function(){
 	$("#name").focus();
 });
@@ -458,13 +514,19 @@ $("#name").keyup(function(){
 
 <body>
 
-<?php if($e){ //escreve mensagens de erro ?>
+<?php
+// ====================================== 
+// Escreve mensagens de erro do servidor quando nao temos Ajax
+	if($e){ ?>
 	<ul id="alert" title="Fechar">
 	<?=$e?>
 	</ul>
 <? } ?>
 
-<?php if($logo){ //insere logo ?>
+<?php
+// ====================================== 
+// Logo
+	if($logo){?>
 	<img src="<?=$logo?>" alt="Logotipo" />
 <? } ?>
 
@@ -473,10 +535,13 @@ $("#name").keyup(function(){
 <? } ?>
 
 <? if($h2){ ?>
-	<h2>O novo site <strong><?=$sitename?></strong> está quase pronto.<? if($when){?> <br />Previsão de lançamento em <?=$when?><? }?></h2>
+	<h2>O novo site <strong><?=$siteName?></strong> está quase pronto.<? if($when){?> <br />Previsão de lançamento em <?=$when?><? }?></h2>
 <? } ?>
 
-<?php if($sent==false) { //oculta form depois do sucesso ?>
+<?php
+// ====================================== 
+// Oculta form depois do sucesso sem Ajax
+	if($sent==false) { ?>
 
 <form action="index.php" method="post">
 	<fieldset>
@@ -491,7 +556,7 @@ $("#name").keyup(function(){
 			<? } ?>
 		</li>
 		<li>
-			<label for="name">Nome <? if($nreq==false) {?><span class="opc">(Opcional)</span><? } ?></label>
+			<label for="name">Nome <? if($nameReq==false) {?><span class="opc">(Opcional)</span><? } ?></label>
 			<input type="hidden" name="token" id="token" value="<?=$secret;?>" />
 			<input type="text" name="name" id="name" value="<?=$name?>" />
 		</li>
@@ -504,6 +569,22 @@ $("#name").keyup(function(){
 	<p>Oba! Você está inscrito e será avisado no lançamento.</p>
 <? } ?>
 
+<?php
+// ====================================== 
+// Google Analytics
+	if($gaUA) { ?>
+		<script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', '<?=$gaUA?>']);
+			_gaq.push(['_trackPageview']);
+			
+			(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+		</script>
+<? } //analytics ?>
 </body>
 </html>
 <? } //nao escrever pagina no ajax ?>
